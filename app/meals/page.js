91910a2398b928c930090536051f9b8815af30cd
Leshista <a.leshista@gmail.com>
2024-05-07 19:14:57 +0300

    Off for today; Followed the tutorial more, added db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,28 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 const MealsPage = () => {
-    return <h1>I am MealsPage</h1>;
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={classes.highlight}>by you</span>
+                    <p>
+                        Choose your favourite recipe and cook it yourself. It is
+                        easy and fun!
+                    </p>
+                    <p className={classes.cta}>
+                        <Link href="/meals/share">Share</Link>
+                    </p>
+                </h1>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
+    );
 };
 
 export default MealsPage;
